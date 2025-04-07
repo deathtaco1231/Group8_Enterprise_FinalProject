7d4e36e1ddf3ceb1d88edc0053c70a3039150f3c
Julian Cumming <jcumming@conestogac.on.ca>
2025-04-07 10:33:48 -0400

    Add project files.

---
+++ Group8_Enterprise_FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group8_Enterprise_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

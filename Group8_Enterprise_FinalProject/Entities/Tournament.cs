a1a2e47010d53698813e47028717ec7e35a3747b
Bradley Krawchyk <Bkrawchyk4812@conestogac.on.ca>
2025-04-12 12:25:23 -0400

    Edit Page and Tournament Entity + Model (Untested)

---
+++ Group8_Enterprise_FinalProject/Entities/Tournament.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Group8_Enterprise_FinalProject.Entities
+{
+    public class Tournament
+    {
+        //PK
+        public int TournamentId { get; set; }
+
+        //Name (required)
+        [Required(ErrorMessage = "Please enter a tournament name")]
+        public string? Name { get; set; }
+
+        //Game (required)
+        [Required(ErrorMessage = "Please enter a game")]
+        public string? Game { get; set; }
+
+        //Number of players per team (required)
+        [Required(ErrorMessage = "Please enter the number of players per team")]
+        public int NumPlayersPerTeam { get; set; }
+
+        //Start date and time (required)
+        [Required(ErrorMessage = "Please enter the start date and time")]
+        public DateTime StartDateTime { get; set; }
+
+        //Number of games to be played (required)
+        [Required(ErrorMessage = "Please enter the number of games to be played")]
+        public int NumGames { get; set; }
+    }
+}

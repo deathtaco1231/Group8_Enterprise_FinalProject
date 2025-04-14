6c89b4e888f89f24910e70658468e7b545e91658
Julian Cumming <jcumming@conestogac.on.ca>
2025-04-13 21:29:35 -0400

    - Still working on API, getting most up to date object defns still

--- Group8_Enterprise_FinalProject/API's/TournamentsAPIController.cs
+++ Group8_Enterprise_FinalProject/API's/TournamentsAPIController.cs
@@ -8,7 +8,7 @@ using Group8_Enterprise_FinalProject.Messages;
 namespace Group8_Enterprise_FinalProject.API_s
 {
     /// <summary>
-    /// Controller class for the routes of the Quotes API managed by this ASP.NET Core app
+    /// Controller class for the routes of the Tournaments API managed by this ASP.NET Core app
     /// </summary>
     [ApiController]
     [Route("tournaments-api")]
@@ -46,44 +46,44 @@ namespace Group8_Enterprise_FinalProject.API_s
         /// Route for using API to retrieve list of all tournaments held by the app's database
         /// </summary>
         /// <returns></returns>
-        //[HttpGet("/tournaments")]
-        //public IActionResult GetAllTournaments()
-        //{
-        //    var quotes = _tournamentDbContext.Tournaments
-        //        .Include(q => q.Teams)
-        //        .ThenInclude(ta => ta.TeamId)
-        //        .ToList();
+        [HttpGet("/tournaments")]
+        public IActionResult GetAllTournaments()
+        {
+            var quotes = _tournamentDbContext.Tournaments
+                .Include(q => q.Teams)
+                .ThenInclude(ta => ta.TeamId)
+                .ToList();
 
-        //    if (quotes == null || quotes.Count == 0) // Fallback to safe return of no content if no quotes exist
-        //    {
-        //        return NoContent();
-        //    }
+            if (quotes == null || quotes.Count == 0) // Fallback to safe return of no content if no quotes exist
+            {
+                return NoContent();
+            }
 
-        //    // Populate quote details object with contents of quote and URL for that quote's get by ID route for each quote
-        //    // (sorted here since this is what impacts the display order, NOT the previous sort)
-        //    var quotesDetailsObj = quotes.Select(q => new QuoteDetails
-        //    {
-        //        QuoteID = q.QuoteID,
-        //        Content = q.Content,
-        //        Author = q.Author,
-        //        Likes = q.Likes,
-        //        Tags = q.TagAssignments.Select(ta => ta.Tag.TagName).ToList(),
-        //        Url = GenerateFullUrl($"/quotes/{q.QuoteID}")
-        //    }).ToList(); // Resolve to list of quote details objects
+            // Populate quote details object with contents of quote and URL for that quote's get by ID route for each quote
+            // (sorted here since this is what impacts the display order, NOT the previous sort)
+            var quotesDetailsObj = quotes.Select(q => new QuoteDetails
+            {
+                QuoteID = q.QuoteID,
+                Content = q.Content,
+                Author = q.Author,
+                Likes = q.Likes,
+                Tags = q.TagAssignments.Select(ta => ta.Tag.TagName).ToList(),
+                Url = GenerateFullUrl($"/quotes/{q.QuoteID}")
+            }).ToList(); // Resolve to list of quote details objects
 
-        //    DateTime? lastModified; // Placing last modified obj here for greater scope (instead of return calls inside both the if and else blocks)
-        //    if (quotes.Max(q => q.LastModified) == null)
-        //    {
-        //        lastModified = new DateTime(1970, 1, 1); // Safe default if no quotes exist (therefore no datetimes to get a max from)
-        //    }
-        //    else
-        //    {
-        //        // Since lastModified is nullable, this doesnt complain like it would if it wasn't nullable
-        //        lastModified = quotes.Max(q => q.LastModified);
-        //    }
+            DateTime? lastModified; // Placing last modified obj here for greater scope (instead of return calls inside both the if and else blocks)
+            if (quotes.Max(q => q.LastModified) == null)
+            {
+                lastModified = new DateTime(1970, 1, 1); // Safe default if no quotes exist (therefore no datetimes to get a max from)
+            }
+            else
+            {
+                // Since lastModified is nullable, this doesnt complain like it would if it wasn't nullable
+                lastModified = quotes.Max(q => q.LastModified);
+            }
 
-        //    return Ok(new { quotes = quotesDetailsObj, quotesLastModified = lastModified });
-        //}
+            return Ok(new { quotes = quotesDetailsObj, quotesLastModified = lastModified });
+        }
 
         private string GenerateFullUrl(string path) // Taken from Tasks example, makes it easy to get URL's in correct form (without weird extra / issue at end of URLs that I was getting before)
         {
---
+++ Group8_Enterprise_FinalProject/Messages/TournamentDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace PA3_EnterpriseAppDev_JulianCumming.Messages
+{
+    /// <summary>
+    /// Class for holding necessary details of quote (stripped down slightly for associated tag names only) for sending to client,
+    /// and URL for quote's ID route
+    /// </summary>
+    public class TournamentDetails
+    {
+        public int TournamentID { get; set; }
+
+    }
+}
